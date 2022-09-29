ef9d83bec76d2afa76f0074605d0ebc659b3b5cb
Peeyush Chandra Das <peeyushdas27@gmail.com>
2022-09-29 20:51:51 +0530

    third

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ import './Resume.scss';
 const Resume = () => {
   return (
     <div className='resumeimg'>
-      <a href='https://drive.google.com/file/d/1ah3_uHK6UaQ2E6BI_a7Wh5-x5dNdXL6m/view?usp=sharing' target="_blank">
+      <a href='https://drive.google.com/file/d/1cJiCmZEkgBZGGO22WwPLX0672vvPRhuB/view?usp=sharing' target="_blank">
       <img src={Resumep} alt='Peeyush resume'/>
       </a>
     </div>
